daf0f94533ade72cd778dc9ad7af34f9b2e50127
ryuya416 <h2luv0r1a8@icloud.com>
2021-10-22 11:37:17 +0900

    ✨ メッセージ表示機能の実装

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,9 +3,11 @@ import axios from "axios";
 
 import { User } from "../types/api/user";
 import { useHistory } from "react-router";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const history = useHistory();
+  const { showMessage } = useMessage();
 
   const [loading, setLoading] = useState(false);
 
@@ -17,15 +19,21 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            showMessage({ title: "ログインしました", status: "success" });
             history.push("/home");
           } else {
-            alert("ユーザーが見つかりません");
+            showMessage({
+              title: "ユーザーが見つかりません",
+              status: "error",
+            });
           }
         })
-        .catch(() => alert("ログインできません"))
+        .catch(() =>
+          showMessage({ title: "ログインできません", status: "error" })
+        )
         .finally(() => setLoading(false));
     },
-    [history]
+    [history, showMessage]
   );
   return { login, loading };
 };
